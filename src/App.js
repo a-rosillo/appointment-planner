3742fc001105602846c1f3518b4db970a8941298
a-rosillo <128966633+a-rosillo@users.noreply.github.com>
2023-06-06 07:29:42 +0100

    added contact and appointment states and function to add contact.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [ contacts, setContacts ] = useState([]);
+  const [ appointments, setAppointments ] = useState([]);
 
+  const addContact = (name, phone, email) => {
+    const contact = {
+      name: name,
+      phone: phone,
+      email: email
+    }
+    setContacts((prev) => [...prev, contact])
+  }
+  
   /*
   Implement functions to add data to
   contacts and appointments
